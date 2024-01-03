56912def223a58d901ed9502e829708e0bf6b524
Andrea Barnabo <andrea.barnabo92@gmail.com>
2024-01-03 18:20:38 +0100

    add view

--- wedding-django/wedding/lista_nozze/views.py
+++ wedding-django/wedding/lista_nozze/views.py
@@ -5,3 +5,9 @@ from .serializers import ItemSerializer
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+
+from django.shortcuts import render
+
+def item_list(request):
+    items = Item.objects.all()
+    return render(request, 'lista_nozze/item_list.html', {'items': items})
--- wedding-django/wedding/wedding/urls.py
+++ wedding-django/wedding/wedding/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from lista_nozze.views import item_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('lista_nozze.urls')),
+    path('', item_list, name='item_list'),
 ]
