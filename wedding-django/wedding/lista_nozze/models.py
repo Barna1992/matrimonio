0970705b382ba5aaed5bd1bb60d5fd10df146bdf
Andrea Barnabo <andrea.barnabo92@gmail.com>
2024-03-11 12:31:32 +0100

    avanzamenti

--- wedding-django/wedding/lista_nozze/models.py
+++ wedding-django/wedding/lista_nozze/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
+CATEGORY_CHOICES = [
+    ('travel', 'travel'),
+    ('home', 'home'),
+]
+
 class Item(models.Model):
     title = models.CharField(max_length=200)
     price = models.IntegerField()
     picture = models.ImageField(upload_to='pictures/', blank=True, null=True)
     quantity = models.IntegerField()
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='travel') 
 
     def __str__(self):
         return self.title
