c099777912c42958d68ccb652808892074b29607
Andrea Barnabo <andrea.barnabo92@gmail.com>
2024-03-17 19:12:10 +0100

    decremento valori sul be

--- wedding-django/wedding/lista_nozze/views.py
+++ wedding-django/wedding/lista_nozze/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from .models import Item, Friend, Food
 from .serializers import ItemSerializer, FriendSerializer, FoodSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
@@ -9,6 +11,22 @@ class ItemViewSet(viewsets.ModelViewSet):
 class FriendViewSet(viewsets.ModelViewSet):
     queryset = Friend.objects.all()
     serializer_class = FriendSerializer
+
+    def create(self, request, *args, **kwargs):
+        items = request.data.pop('string_ids', [])
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            for item in items:
+                item_id, item_quantity = item.split('-')
+                current = Item.objects.get(id=item_id)
+                current.quantity -= int(item_quantity)
+                current.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 
 class FoodViewSet(viewsets.ModelViewSet):
     queryset = Food.objects.all()
