3c6be9d54b5d7713b8d234a242bc2805f6873497
Andrea Barnabo <andrea.barnabo92@gmail.com>
2024-03-30 21:59:09 +0100

    modifica a food

--- wedding-django/wedding/lista_nozze/models.py
+++ wedding-django/wedding/lista_nozze/models.py
@@ -29,7 +29,10 @@ class Friend(models.Model):
     
 class Food(models.Model):
     name = models.CharField(max_length=255)
-    comment = models.TextField()
+    allergie = models.TextField()
+    pesce = models.BooleanField(default=True)
+    vegetariano = models.BooleanField(default=False)
+
 
     def __str__(self):
         return self.name
